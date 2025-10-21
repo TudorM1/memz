93ff5770fca6954b6953cb2691c2c5bf2b17cf92
Tvd0r <77831610+TudorM1@users.noreply.github.com>
2025-10-21 17:09:39 +0300

    Refactor Memory class and add memory writing
    Refactored Memory class and main function to include memory writing functionality.

--- main.cpp
+++ main.cpp
@@ -1,93 +1,12 @@
-#include <Tlhelp32.h>
-#include <cstdint>
-#include <string_view>
+#include "memory.h"
+
+int main() {
+	Memory memory;
+	const auto ModuleBaseAdress = memory.GetModuleBaseAddress("urGame.exe");
 
 
-
-// Create a snapshot of all the running process on our computer
-
-class Memory
-{
-private:
-	std::uintptr_t processId = 0;
-	void* processHandle = nullptr;
-
-public:
-
-	// Memory Constructor - used for finding the processId and opening the handle
-	Memory(const std::string_view processName) noexcept
+	while (true)
 	{
-		::PROCESSENTRY32 entry = { };
-		entry.dwSize = sizeof(::PROCESSENTRY32);
-
-		const auto processSnapshot = ::CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
-
-		// Automatically find the right game process
-		while (::Process32Next(processSnapshot, &entry))
-		{
-			if (!processName.compare(entry.szExeFile))
-			{
-				processId = entry.th32ProcessID;
-
-				// Get full modifying acces to the game
-				processHandle = ::OpenProcess(PROCESS_ALL_ACCESS, FALSE, processId);
-				break;
-			}
-		}
-
-		// Close the handle
-		if (processSnapshot)
-			::CloseHandle(processSnapshot);
+		memory.Write<int>(ModuleBaseAdress + 0x123, 99999);
 	}
-
-	// Memory Deconstructor - cleanup by closing the process handle to the game's memory.
-	~Memory()
-	{
-		if (processHandle)
-			::CloseHandle(processHandle);
-	}
-
-
-	// Make GetModelBaseAdress function
-	const std::uintptr_t GetModuleAdress(const std::string_view moduleName) const noexcept
-	{
-		::MODULEENTRY32 entry = { }; 
-		entry.dwSize = sizeof(::MODULEENTRY32);
-
-		const auto processSnapshot = ::CreateToolhelp32Snapshot(TH32CS_SNAPMODULE | TH32CS_SNAPMODULE, processId);
-
-		std::uintptr_t moduleBaseAdress = 0;
-
-		while (::Module32Next(processSnapshot, &entry))
-		{
-			if (!moduleName.compare(entry.szModule))
-			{
-
-				moduleBaseAdress = reinterpret_cast<std::uintptr_t>(entry.modBaseAddr);
-				break;
-			}
-		}
-
-		if (processSnapshot)
-			::CloseHandle(processSnapshot);
-
-		return moduleBaseAdress;
-	}
-
-	// ReadProcessMemory
-	template <typename T>
-	constexpr const T Read(const std::uintptr_t& adress) const noexcept
-	{
-		T value = { };
-		::ReadProcessMemory(processHandle, reinterpret_cast<const void*>(adress), &value, sizeof(T), NULL);
-		return value;
-	}
-
-	// WriteProcessMemory
-	template <typename T>
-	constexpr void Write(const std::uintptr_t& adress, const T& value) const noexcept
-	{
-		::WriteProcessMemory(processHandle, reinterpret_cast<void*>(adress), &value, sizeof(T), NULL);
-	}
-};
-
+}
